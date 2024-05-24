799a57cde3a9702527f36cfb85b89ba3ed33317a
Admael Jabes <admael.silva15@gmail.com>
2024-05-24 18:04:34 -0300

    Evolução do segundo formulário, validação dos inputs e habilitado botão para prosseguir. Em seguida, evolução para o terceiro formulário, validação dos inputs e habilitado botão para concluir.

--- js/index.js
+++ js/index.js
@@ -3,21 +3,40 @@ const $stepDescription = $('#step-description');
 const $stepOne = $('.step.one');
 const $stepTwo = $('.step.two');
 const $stepThree = $('.step.three');
+const $title = $('#title');
 const $containerBtnFormOne = $('#containerBtnFormOne');
 const $btnFormOne = $('#btnFormOne');
+const $containerBtnFormTwo = $('#containerBtnFormTwo');
+const $btnFormTwo = $('#btnFormTwo');
+const $containerBtnFormThree = $('#containerBtnFormThree');
+const $btnFormThree = $('#btnFormThree');
 const $inputNome = $('#nome');
 const $inputSobrenome = $('#sobrenome');
 const $inputDataNascimento = $('#dataNascimento');
 const $inputEmail = $('#email');
 const $inputMinibio = $('#minibio');
+const $inputEndereco = $('#endereco');
+const $inputComplemento = $('#complemento');
+const $inputCidade = $('#cidade');
+const $inputCep = $('#cep');
+const $inputHabilidades = $('#habilidades');
+const $inputPontosForte = $('#pontosForte');
 
 let nomeValido = false;
 let sobrenomeValido = false;
 let dataNascimentoValido = false;
 let emailValido = false;
+let enderecoValido = false;
+let cidadeValida = false;
+let cepValido = false;
+let habilidadesValida = false;
+let pontosFortesValido = false;
+
 
 const minLengthText = 2;
+const minLengthTextArea = 10;
 const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const cepRegex = /^([\d]{2})([\d]{3})([\d]{3})|^[\d]{2}.[\d]{3}-[\d]{3}/
 
 function validarInput(element, minLength, maxLength, regex) {
     const closest = $(element).closest('.input-data');
@@ -35,9 +54,7 @@ function validarInput(element, minLength, maxLength, regex) {
 
 function validaEmail(element) {
     const closest = $(element).closest('.input-data');
-    if (!element.value || !element.value.toLowerCase().match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    )) {
+    if (!element.value || !element.value.toLowerCase().match(emailRegex)) {
         closest.addClass('error');
         return false;
     }
@@ -49,6 +66,7 @@ function validaFormularioUm() {
     if (nomeValido && sobrenomeValido && emailValido && dataNascimentoValido) {
         $containerBtnFormOne.removeClass('disabled');
         $btnFormOne.removeClass('disabled');
+        $btnFormOne.off('click').on('click', iniciarFormulario2);
     } else {
         $containerBtnFormOne.addClass('disabled');
         $btnFormOne.addClass('disabled');
@@ -56,9 +74,87 @@ function validaFormularioUm() {
     }
 }
 
+function iniciarFormulario2(){
+    $stepText.text('Passo 2 de 3 - Dados de correspondência');
+    $stepDescription.text('Precisamos destes dados para que possamos entrar em contaro se necessário.');
+    $stepOne.hide();
+    $stepTwo.show();
+
+    $inputEndereco.keyup(function(){
+        enderecoValido = validarInput(this, minLengthTextArea);
+        validaFormularioDois();
+    });
+
+    $inputCidade.keyup(function(){
+        cidadeValida = validarInput(this, minLengthText);
+        validaFormularioDois();
+    });
+
+    $inputCep.keyup(function(){
+        this.value = this.value.replace(/\D/g,'');
+        cepValido = validarInput(this, null, null, cepRegex)
+        if(cepValido){
+            this.value = this.value.replace(cepRegex,"$1.$2-$3")
+        }
+        validaFormularioDois();
+    });
+
+    $inputComplemento.keyup(function(){
+        validaFormularioDois();
+    });
+}
+
+function validaFormularioDois(){
+    if(enderecoValido && cidadeValida && cepValido){
+        $containerBtnFormTwo.removeClass('disabled');
+        $btnFormTwo.removeClass('disabled');
+        $btnFormTwo.off('click').on('click', iniciarFormularioTres);
+    }else{
+        $containerBtnFormTwo.addClass('disabled');
+        $btnFormTwo.addClass('disabled'); 
+        $btnFormTwo.off('click'); 
+    }
+}
+
+function iniciarFormularioTres(){
+    $stepText.text('Passo 3 de 3 - Conte-nos sobre você:');
+    $stepDescription.text('Não economize nas palavras, aqui é onde você pode se destacar.'); 
+    $stepThree.show();
+    $stepTwo.hide();
+    
+    $inputHabilidades.keyup(function(){
+        habilidadesValida = validarInput(this, minLengthTextArea);
+        validaFormularioTres();
+    });
+
+    $inputPontosForte.keyup(function(){
+        pontosFortesValido = validarInput(this, minLengthTextArea);
+        validaFormularioTres();
+    });
+}
+
+function validaFormularioTres(){
+    if(habilidadesValida && pontosFortesValido){
+        $containerBtnFormThree.removeClass('disabled');
+        $btnFormThree.removeClass('disabled');
+        $btnFormThree.off('click').on('click', finalizarFormulario);
+    } else{
+        $containerBtnFormThree.addClass('disabled');
+        $btnFormThree.addClass('disabled');
+        $btnFormThree.off('click');
+    }
+}
+
+function finalizarFormulario(){
+    $stepThree.hide();
+    $stepDescription.hide();
+    $title.text('Muito Obrigado pela sua incrição!');
+    $stepText.text('Entraremos em contato assim que possivél, nosso prazo médio é de 5 dias. Fique atento na sua caixa de email');
+}
+
 function init() {
     $stepText.text('Passo 1 de 3 - Dados pessoais');
-    $stepDescription.text('Descreva seus dados para te conhecermos melhor.');
+    $stepDescription.text('Descreva seus dados para te conhecermos melhor:');
     $stepTwo.hide();
     $stepThree.hide();
 
